6852e605c713c4337c49a8ce2b42c9e99c9623cc
Tobias Ryttlinger <tosryr@gmail.com>
2020-09-08 15:26:23 +0200

    .

--- Settlers of Ai/Assets/Scripts/HexGrid.cs
+++ Settlers of Ai/Assets/Scripts/HexGrid.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class HexGrid : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public int width = 6;
+	public int height = 6;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public HexCell cellPrefab;
+	HexCell[] cells;
+
+	void Awake()
+	{
+		cells = new HexCell[height * width];
+
+		for (int z = 0, i = 0; z < height; z++)
+		{
+			for (int x = 0; x < width; x++)
+			{
+				CreateCell(x, z, i++);
+			}
+		}
+	}
+
+	void CreateCell(int x, int z, int i)
+	{
+		Vector3 position;
+		position.x = x * 10f;
+		position.y = 0f;
+		position.z = z * 10f;
+
+		HexCell cell = cells[i] = Instantiate<HexCell>(cellPrefab);
+		cell.transform.SetParent(transform, false);
+		cell.transform.localPosition = position;
+	}
 }
