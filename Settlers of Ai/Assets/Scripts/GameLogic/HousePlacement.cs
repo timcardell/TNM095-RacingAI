73c025f05b1d3734b120bed26417efbb1035dfd9
Tobias Ryttlinger <tosryr@gmail.com>
2020-09-22 10:28:08 +0200

    asd

---
+++ Settlers of Ai/Assets/Scripts/GameLogic/HousePlacement.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HousePlacement : MonoBehaviour
+{
+    // the Equip prefab - required for instantiation
+    public GameObject equipPrefab;
+    public GameObject upgradePrefab;
+    private Grid GameGrid;
+
+
+    // list that holds all created objects - deleate all instances if desired
+    public List<GameObject> createdObjects = new List<GameObject>();
+    public Player currentPlayer;
+    private float minX, maxX, minY, maxY;
+
+    public void Awake()
+    {
+        GameGrid = FindObjectOfType<Grid>();
+    }
+
+    void Start()
+    {
+        // get the screen bounds
+        float camDistance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        Vector2 bottomCorner = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, camDistance));
+        Vector2 topCorner = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, camDistance));
+
+        minX = bottomCorner.x;
+        maxX = topCorner.x;
+        minY = bottomCorner.y;
+        maxY = topCorner.y;
+    }
+
+
+
+    public void CreateObject()
+    {
+        // a prefab is need to perform the instantiation
+        if (equipPrefab != null)
+        {
+            // get a random postion to instantiate the prefab - you can change this to be created at a fied point if desired
+            Vector3 position = new Vector3(Random.Range(minX + 0.5f, maxX - 0.5f), Random.Range(minY + 0.5f, maxY - 0.5f), 0);
+
+            // instantiate the object
+            GameObject go = Instantiate(equipPrefab, position, Quaternion.identity) as GameObject; ;
+
+            createdObjects.Add(go);
+            currentPlayer.Towns.Add(go);
+        }
+    }
+
+    public void upgradeToTown()
+    {
+
+        if(currentPlayer.Stone >= 2 && currentPlayer.Wheat >= 3 && currentPlayer.Towns.Count > 0)
+        {
+            currentPlayer.Towns.RemoveAt(currentPlayer.Towns.Count);
+            // get a random postion to instantiate the prefab - you can change this to be created at a fied point if desired
+            Vector3 position = new Vector3(Random.Range(minX + 0.5f, maxX - 0.5f), Random.Range(minY + 0.5f, maxY - 0.5f), 0);
+
+            // instantiate the object
+            GameObject go = (GameObject)Instantiate(upgradePrefab, position, Quaternion.identity);
+
+            currentPlayer.Settlements.Add(go);
+
+        }
+
+
+
+    }
+
+}
---
+++ Settlers of Ai/Assets/Scripts/GameLogic/Score.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Text ScoreText;
+    public Player CurrentPlayer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        ScoreText.text = "Score: " + CurrentPlayer.score;
+    }
+}
